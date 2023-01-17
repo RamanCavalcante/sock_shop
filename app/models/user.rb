1cd7bbaa223e47f907b09c558445aba5531d55fb
RamanCavalcante <cavalcanteraman2@gmail.com>
2023-01-17 11:20:58 -0300

    adicionando enum role no model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :name, :phone, presence: true
+  validates :name, :phone, presence: true
+
+  enum role: {user: 'user', admin: 'admin'}
+  
 end
